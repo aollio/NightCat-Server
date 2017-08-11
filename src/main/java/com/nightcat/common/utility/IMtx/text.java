fec2e369e71a8dc0cda0df5380749aa4e68ffae7
Qmrhan <18827526631>
2017-08-11 12:46:03 +0800

    webhook

---
+++ src/main/java/com/nightcat/common/utility/IMtx/text.java
@@ -0,0 +1,47 @@
+package com.nightcat.common.utility.IMtx;
+
+import com.nightcat.common.utility.wangyi.CheckSumBuilder;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class text {
+
+    public static void main(String[] args) throws Exception{
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        String url = "https://api.netease.im/nimserver/user/create.action";
+        HttpPost httpPost = new HttpPost(url);
+
+        String appKey = "47045ca57e2ca57f805cb24563e34160";
+        String appSecret = "818942ff3fac";
+        String nonce =  "12345";
+        String curTime = String.valueOf((new Date()).getTime() / 1000L);
+        String checkSum = CheckSumBuilder.getCheckSum(appSecret, nonce ,curTime);//参考 计算CheckSum的java代码
+
+        // 设置请求的header
+        httpPost.addHeader("AppKey", appKey);
+        httpPost.addHeader("Nonce", nonce);
+        httpPost.addHeader("CurTime", curTime);
+        httpPost.addHeader("CheckSum", checkSum);
+        httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // 设置请求的参数
+        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+        nvps.add(new BasicNameValuePair("accid", "helloworld"));
+        httpPost.setEntity(new UrlEncodedFormEntity(nvps, "utf-8"));
+
+        // 执行请求
+        HttpResponse response = httpClient.execute(httpPost);
+
+        // 打印执行结果
+        System.out.println(EntityUtils.toString(response.getEntity(), "utf-8"));
+    }
+}
